06a02db6a3c612fb55c1333c9a2cec7e14934156
Jackson Demaree <jackson.demaree@gmail.com>
2017-10-27 11:29:34 -0400

    Zuul fix

--- src/main/java/com/revature/service/HelloService.java
+++ src/main/java/com/revature/service/HelloService.java
@@ -13,7 +13,7 @@ public class HelloService {
 	@HystrixCommand(fallbackMethod = "reliable")
 	public String hello() {
 	    RestTemplate restTemplate = new RestTemplate();
-	    URI uri = URI.create("http://18.221.160.51:8085/api/hystrixdemo/hello");
+	    URI uri = URI.create("/api/hystrixdemo/hello");
 
 	    return restTemplate.getForObject(uri, String.class);
 	}
